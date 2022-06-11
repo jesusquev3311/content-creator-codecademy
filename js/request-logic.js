c0dc2855364fd9c7c6231b2a6d7acb6c40a5032e
jesusquev3311 <jesusquev3311@gmail.com>
2022-06-11 12:42:21 -0400

    updating getContentType function

--- js/request-logic.js
+++ js/request-logic.js
@@ -1,8 +1,17 @@
 // We at Content Creators know this code is useful for getting the
 // extension off of the supplied filename, but we can't figure out the rest of
 // the function to use it! We hope this is useful to you!
-
+const FILE_TYPE = {
+  html: "text/html",
+  css: "text/css",
+  jpeg: "image/jpeg",
+  png: "image/png",
+  txt: "text/plain",
+  default: "File type undefined",
+}
 
 function getContentType(filename) {
   const extension = filename.match(/.*\.([^\.]*)$/)[1];
+
+  return FILE_TYPE[extension] || FILE_TYPE.default;
 }
